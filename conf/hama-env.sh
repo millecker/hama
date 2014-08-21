5f9120b722f45a78ba0d8726f04e66936b04151e
millecker <martin.illecker@gmail.com>
2014-08-21 09:33:31 +0200

    Merge hama-env.sh

--- conf/hama-env.sh
+++ conf/hama-env.sh
@@ -22,12 +22,12 @@
 # Set environment variables here.
 
 # The java implementation to use.  Required.
-#export JAVA_HOME=/usr/lib/jvm/java-6-sun
-#export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home
+# export JAVA_HOME=/usr/lib/jvm/java-7-oracle
+# export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home
 export JAVA_HOME=/usr/local/java/jdk
 
 # Where log files are stored.  $HAMA_HOME/logs by default.
-# export HAMA_LOG_DIR=${HAMA_HOME}/logs
+export HAMA_LOG_DIR=${HAMA_HOME}/logs
 
 # The maximum amount of heap to use, in MB. Default is 1000.
 # export HAMA_HEAPSIZE=1000
